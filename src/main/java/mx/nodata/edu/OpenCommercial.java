8884c357e8f561d72c1f5e1ceb694f3ded6963c7
Victor Arana Rodriguez <victor_arana@hotmail.com>
2014-08-31 14:44:39 -0500

    Homework 01 Solved using ArrayList and Iterators

--- src/main/java/mx/nodata/edu/OpenCommercial.java
+++ src/main/java/mx/nodata/edu/OpenCommercial.java
@@ -1,9 +1,13 @@
 package mx.nodata.edu;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ListIterator;
+
 /* OpenCommercial.java */
 
-import java.net.*;
-import java.io.*;
 
 /**  A class that provides a main function to read five lines of a commercial
  *   Web page and print them in reverse order, given the name of a company.
@@ -20,16 +24,48 @@ class OpenCommercial {
    */
   public static void main(String[] arg) throws Exception {
 
-    BufferedReader keyboard;
-    String inputLine;
-
-    keyboard = new BufferedReader(new InputStreamReader(System.in));
-
+	InputStreamReader keyboardInputStreamReader;
+	InputStreamReader urlInputStreamReader;
+	BufferedReader keyboard;
+	BufferedReader website;
+	ArrayList<String> websiteLine;
+	ListIterator<String> listIterator;
+	
+	String inputLine = "";
+	String urlString = ""; 
+	int numberOfLines = 5;
+    
+    // 1. Create a BufferedReader to read text lines  from the console
+    keyboardInputStreamReader = new InputStreamReader(System.in);
+    keyboard = new BufferedReader(keyboardInputStreamReader);
+    
+    // 2. Prompts the user for the name X of a company
     System.out.print("Please enter the name of a company (without spaces): ");
     System.out.flush();        /* Make sure the line is printed immediately. */
     inputLine = keyboard.readLine();
-
-    /* Replace this comment with your solution.  */
-
+    
+    // 3. Prints the input entered
+    System.out.println();
+    System.out.println(inputLine);
+    
+    // 4. Create a BufferedReader to read content from a URL
+    urlString = "http://www." + inputLine + ".com";
+    URL url = new URL(urlString);
+    urlInputStreamReader = new InputStreamReader(url.openStream());
+    website = new BufferedReader(urlInputStreamReader);
+    
+    // 5. Initialize the list
+    websiteLine = new ArrayList<String>();
+    
+    // 6. Read the lines using the BufferedReader and add to a list 
+    for(int i = 0; i < numberOfLines; i++){
+    	websiteLine.add(website.readLine());
+    }
+    
+    // 7. Print the lines in reverse order
+    listIterator = websiteLine.listIterator(websiteLine.size());
+    while(listIterator.hasPrevious()){
+      System.out.println(listIterator.previous());
+    }
   }
 }
